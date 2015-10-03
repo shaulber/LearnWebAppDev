9a9501cb879619c9a8142fa140e8774816a4cbad
Shaul Berkovich <shaulber@gmail.com>
2015-10-03 18:29:39 +0300

    Style

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,3 +1,14 @@
 /**
  * Created by shaulberkovich on 10/3/15.
  */
+
+var main = function() {
+    "use strict";
+    $(".comment-input button").on("click",function(event){
+        if ($(".comment-input input").val() !== ""){
+            var $new_comment = $("<p>").text($(".comment-input input").val());
+            $(".comments").append($new_comment);
+        }
+    });
+};
+$(document).ready(main);
