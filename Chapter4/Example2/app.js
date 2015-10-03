574c826ac719f05935748756a2f528e197d0b914
Shaul Berkovich <shaulber@gmail.com>
2015-10-03 21:39:30 +0300

    Style

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -4,13 +4,27 @@
 
 var main = function() {
     "use strict";
+
     $(".comment-input button").on("click",function(event){
+
         if ($(".comment-input input").val() !== ""){
-            var $new_comment = $("<p>").text($(".comment-input input").val());
-            $(".comments").append($new_comment);
-            $(".comment-input input").val("");
+           addCommentFromInput()
         }
 
     });
+    $(".comment-input input").on("keypress",function(event){
+        if (event.keyCode === 13){
+            addCommentFromInput()
+        }
+    });
+
+    var addCommentFromInput = function (){
+        var $new_comment;
+        $new_comment = $("<p>").text($(".comment-input input").val());
+        $new_comment.hide();
+        $(".comments").append($new_comment);
+        $new_comment.fadeIn();
+        $(".comment-input input").val("");
+    }
 };
 $(document).ready(main);
