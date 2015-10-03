7c8c8dd0f46560562a08decb60ef63e1048227d8
Shaul Berkovich <shaulber@gmail.com>
2015-10-03 18:52:18 +0300

    Style

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -8,7 +8,9 @@ var main = function() {
         if ($(".comment-input input").val() !== ""){
             var $new_comment = $("<p>").text($(".comment-input input").val());
             $(".comments").append($new_comment);
+            $(".comment-input input").val("");
         }
+
     });
 };
 $(document).ready(main);
